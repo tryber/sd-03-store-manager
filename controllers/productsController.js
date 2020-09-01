c20cde181378f38c2f44038f624567755676feb6
Hfreitas <hfreitas86@gmail.com>
2020-09-01 20:04:22 -0300

    requisito 1?

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,25 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
+const productsServices = require('../services/productsServices');
 
 const products = Router();
 
-products.post('/products', rescue(async (req, res, next) => {
-  const { name, quantity } = req.body;
-}));
+products.post(
+  '/',
+  rescue(async (req, res, next) => {
+    const { name, quantity } = req.body;
+    try {
+      const product = await productsServices.createProduct(name, quantity);
+      if (product.message) throw new Error({ message: product.message });
+      return res.status(201).json(products);
+    } catch (error) {
+      const err = {
+        status: 422,
+        payload: { err: { code: 'invalid_data', message: error.message } },
+      };
+      return next(err);
+    }
+  }),
+);
 
 module.exports = products;
--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const productsController = require('./controllers/productsController');
+
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/products', productsController);
+
+app.use((err, _, res, _next) =>
+  (err
+    ? res.status(err.status).json(err.payload)
+    : res.status(500).json({ message: 'Internal error' })));
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 
-const PORT = process.env.PORT || 3000;
+const { PORT = 3000 } = process.env;
 app.listen(PORT, () => console.log(`Listen port ${PORT}`));
