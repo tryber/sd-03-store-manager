42c2736be045f6f02d46d2e064a0c5632c73f805
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-09-02 19:56:36 -0300

    CC

--- services/helpers.js
+++ services/helpers.js
@@ -1,20 +1,26 @@
-const invalid_data = 'invalid_data';
+const invalidData = 'invalid_data';
+
+const getErrorObject = (code, message) => ({ err: { code, message } });
 
 const validateProductData = (name, quantity) => {
-  if (name && name.length < 5)
-    return getErrorObject(invalid_data, '"name" length must be at least 5 characters long');
+  if (name && name.length < 5) {
+    return getErrorObject(invalidData, '"name" length must be at least 5 characters long');
+  }
 
-  if (quantity <= 0)
-    return getErrorObject(invalid_data, '"quantity" must be larger than or equal to 1');
+  if (quantity <= 0) {
+    return getErrorObject(invalidData, '"quantity" must be larger than or equal to 1');
+  }
 
-  if (typeof quantity !== 'number')
-    return getErrorObject(invalid_data, '"quantity" must be a number');
-
+  if (typeof quantity !== 'number') {
+    return getErrorObject(invalidData, '"quantity" must be a number');
+  }
   return true;
 };
 
 const validateId = (id, message) => {
-  if (id.length < 24) return getErrorObject(invalid_data, message);
+  if (id.length < 24) {
+    return getErrorObject(invalidData, message);
+  }
 
   return true;
 };
@@ -24,8 +30,6 @@ const validateSaleData = (products) =>
     ({ productId, quantity }) =>
       quantity >= 1 && typeof quantity === 'number' && productId.length === 24,
   );
-
-const getErrorObject = (code, message) => ({ err: { code, message } });
 
 module.exports = {
   validateProductData,
