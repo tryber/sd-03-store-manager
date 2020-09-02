bfa8c7520068935260c5315421c5cfae4024fd2a
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-09-02 16:36:36 -0300

    Correção conexão banco teste

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,15 @@
 const { MongoClient } = require('mongodb');
 require('dotenv/config');
 
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
 const connect = () =>
-  MongoClient.connect(process.env.MONGO_DB_URL, {
+  MongoClient.connect(MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then((connection) => connection.db(process.env.DB_NAME))
+    .then((connection) => connection.db(DB_NAME))
     .catch((err) => {
       console.error(err);
       process.exit(1);
