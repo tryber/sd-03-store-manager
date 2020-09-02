fae8eb4405a2b98f0c55b730285f0b0c4a76e715
Gabriel <dreadpool38@gmail.com>
2020-09-01 21:39:47 -0300

    vai carai

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,21 +1,49 @@
 const Products = require('../models/produtos');
 const Validation = require('../services/validations');
 
-async function func(res, cb, req, status) {
-  const product = await cb(req);
-  res.status(status).send(product);
+async function func(res, cb, req, status, status2) {
+  try {
+    const product = await cb(req);
+    res.status(status).send(product);
+  } catch (error) {
+    res.status(status2).send({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong id format',
+      },
+    });
+  }
+}
+
+async function func2(req, res, cb, cb2) {
+  let id;
+  if (req.params) {
+    id = req.params.id;
+  }
+  const { name, quantity } = req.body;
+  const { message } = await cb(name, quantity);
+  if (message) {
+    throw new Error(message);
+  }
+  if (id) {
+    const product = await cb2(id, { name, quantity });
+    res.status(200).send(product);
+  } else {
+    const product = await cb2({ name, quantity });
+    res.status(201).send(product);
+  }
 }
 
 async function productController(req, res) {
   try {
-    const { name, quantity } = req.body;
-    const { message } = await Validation.validadeNewProduct(name, quantity);
-    if (message) {
-      throw new Error(message);
-    }
-    const product = await Products.createProduct({ name, quantity });
-    console.log(product);
-    res.status(201).send(product);
+    await func2(req, res, Validation.validadeNewProduct, Products.createProduct);
+    // const { name, quantity } = req.body;
+    // const { message } = await Validation.validadeNewProduct(name, quantity);
+    // if (message) {
+    //   throw new Error(message);
+    // }
+    // const product = await Products.createProduct({ name, quantity });
+    // res.status(201).send(product);
   } catch (error) {
     const err = {
       err: {
@@ -36,29 +64,29 @@ async function listProducts(req, res) {
 }
 
 async function getProduct(req, res) {
-  try {
-    await func(res, Products.getProductById, req.params.id, 201);
-  } catch (error) {
-    res.status(422).send({
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong id format',
-      },
-    });
-  }
+  // try {
+  await func(res, Products.getProductById, req.params.id, 201, 422);
+  // } catch (error) {
+  // res.status(422).send({
+  //   err: {
+  //     code: 'invalid_data',
+  //     message: 'Wrong id format',
+  //   },
+  // });
+  // }
 }
 
 async function updateProduct(req, res) {
   try {
-    const { id } = req.params;
-    const { name, quantity } = req.body;
-    const { message } = await Validation.validadeUpdateProduct(name, quantity);
-    if (message) {
-      throw new Error(message);
-    }
-    const product = await Products.updateProduct(id, { name, quantity });
-    console.log(product);
-    res.status(200).send(product);
+    await func2(req, res, Validation.validadeUpdateProduct, Products.updateProduct);
+    // const { id } = req.params;
+    // const { name, quantity } = req.body;
+    // const { message } = await Validation.validadeUpdateProduct(name, quantity);
+    // if (message) {
+    //   throw new Error(message);
+    // }
+    // const product = await Products.updateProduct(id, { name, quantity });
+    // res.status(200).send(product);
   } catch (error) {
     const err = {
       err: {
@@ -70,15 +98,15 @@ async function updateProduct(req, res) {
 }
 
 async function deleteProduct(req, res) {
-  try {
-    await func(res, Products.deleteProduct, req.params.id, 200);
-  } catch (error) {
-    res.status(422).send({
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong id format',
-      },
-    });
-  }
+  // try {
+  await func(res, Products.deleteProduct, req.params.id, 200, 422);
+  // } catch (error) {
+  // res.status(422).send({
+  //   err: {
+  //     code: 'invalid_data',
+  //     message: 'Wrong id format',
+  //   },
+  // });
+  // }
 }
 module.exports = { productController, deleteProduct, listProducts, getProduct, updateProduct };
