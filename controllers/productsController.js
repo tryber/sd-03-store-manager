4fa86bafcc95a415d5b59a39e29311d705a67314
Marco Barbosa <marco.meireles.b@gmail.com>
2020-09-02 20:58:07 -0300

    requisito 1 passando localmente

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,16 @@
 const productModel = require('../model/productModel');
 
-const createProduct = async (req, res) => {
-  const { name, quantity } = req.body;
-  const sameProduct = await productModel.getProductByName(name);
-  return res.send({produtos: sameProduct, quantity});
-  // const result = await productModel.createProductInDB(name, quantity);
-  // return res.send(result);
+const sameProduct = async (name) => {
+  const result = await productModel.getProductByName(name);
+  return result;
+};
+
+const createProduct = async (name, quantity) => {
+  const result = await productModel.createProductInDB(name, quantity);
+  return result;
 };
 
 module.exports = {
+  sameProduct,
   createProduct,
 };
--- services/productService.js
+++ services/productService.js
@@ -1,6 +1,25 @@
 const controllers = require('../controllers');
 
-const createProduct = controllers.productsController.createProduct;
+const createProduct = async (req, res) => {
+  const { name, quantity } = req.body;
+  const sameProduct = await controllers.productsController.sameProduct(name);
+  // validações
+  if (typeof name !== 'string' || name.length < 5) {
+    return res.status(422).send({ err: { message: "\"name\" length must be at least 5 characters long", code: "invalid_data" } });
+  }
+  if (sameProduct) {
+    return res.status(422).send({ err: { message: "Product already exists", code: "invalid_data" } });
+  }
+  if (quantity <= 0) {
+    return res.status(422).send({ err: { message: "\"quantity\" must be larger than or equal to 1", code: "invalid_data" } });
+  }
+  if (typeof quantity !== 'number') {
+    return res.status(422).send({ err: { message: "\"quantity\" must be a number", code: "invalid_data" } });
+  }
+
+  const result = await controllers.productsController.createProduct(name, quantity);
+  return res.status(201).send(result);
+}; 
 
 module.exports = {
   createProduct,
