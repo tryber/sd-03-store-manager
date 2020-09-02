c8ebcd73f66f53a0019516dc65f86c6b85ef86c6
Gabriel <dreadpool38@gmail.com>
2020-09-01 21:41:39 -0300

    asdas

--- controller/productsController.js
+++ controller/productsController.js
@@ -37,13 +37,6 @@ async function func2(req, res, cb, cb2) {
 async function productController(req, res) {
   try {
     await func2(req, res, Validation.validadeNewProduct, Products.createProduct);
-    // const { name, quantity } = req.body;
-    // const { message } = await Validation.validadeNewProduct(name, quantity);
-    // if (message) {
-    //   throw new Error(message);
-    // }
-    // const product = await Products.createProduct({ name, quantity });
-    // res.status(201).send(product);
   } catch (error) {
     const err = {
       err: {
@@ -64,29 +57,12 @@ async function listProducts(req, res) {
 }
 
 async function getProduct(req, res) {
-  // try {
   await func(res, Products.getProductById, req.params.id, 201, 422);
-  // } catch (error) {
-  // res.status(422).send({
-  //   err: {
-  //     code: 'invalid_data',
-  //     message: 'Wrong id format',
-  //   },
-  // });
-  // }
 }
 
 async function updateProduct(req, res) {
   try {
     await func2(req, res, Validation.validadeUpdateProduct, Products.updateProduct);
-    // const { id } = req.params;
-    // const { name, quantity } = req.body;
-    // const { message } = await Validation.validadeUpdateProduct(name, quantity);
-    // if (message) {
-    //   throw new Error(message);
-    // }
-    // const product = await Products.updateProduct(id, { name, quantity });
-    // res.status(200).send(product);
   } catch (error) {
     const err = {
       err: {
@@ -98,15 +74,6 @@ async function updateProduct(req, res) {
 }
 
 async function deleteProduct(req, res) {
-  // try {
   await func(res, Products.deleteProduct, req.params.id, 200, 422);
-  // } catch (error) {
-  // res.status(422).send({
-  //   err: {
-  //     code: 'invalid_data',
-  //     message: 'Wrong id format',
-  //   },
-  // });
-  // }
 }
 module.exports = { productController, deleteProduct, listProducts, getProduct, updateProduct };
