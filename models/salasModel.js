62f983d2a9579a8e8dcb90b18bcbd4c20991a1ec
tales <talestales@gmail.com>
2020-09-02 13:51:08 -0300

    d

--- models/salasModel.js
+++ models/salasModel.js
@@ -1,37 +1,37 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
-// const { connect } = require('./connection');
+const { connect } = require('./connection');
 
-// const getAllSales = async () => connect()
-//   .then((db) => db
-//     .collection('sales')
-//     .find({})
-//     .toArray());
+const getAllSales = async () => connect()
+  .then((db) => db
+    .collection('sales')
+    .find({})
+    .toArray());
 
-// const createSale = async (productId, quantity) => connect()
-//   .then((db) => db.collection('sales').insertOne({ itensSold: { productId, quantity } }))
-//   .then(({ insertedId }) => ({ _id: insertedId, itensSold: { productId, quantity } }));
+const createSale = async (productId, quantity) => connect()
+  .then((db) => db.collection('sales').insertOne({ itensSold: { productId, quantity } }))
+  .then(({ insertedId }) => ({ _id: insertedId, itensSold: { productId, quantity } }));
 
-// const getSaleById = async (id) => connect()
-//   .then((db) => db.collection('sales').findOne(ObjectId(id)));
+const getSaleById = async (id) => connect()
+  .then((db) => db.collection('sales').findOne(ObjectId(id)));
 
-// const deleteSale = async (id) => connect()
-//   .then((db) => db.collection('sales').deleteOne({ _id: ObjectId(id) }));
+const deleteSale = async (id) => connect()
+  .then((db) => db.collection('sales').deleteOne({ _id: ObjectId(id) }));
 
-// const updateSale = async (id, { itensSold: { productId, quantity } }) => connect()
-//   .then((db) => db.collection('sales').updateOne({
-//     _id: ObjectId(id)
-//   }, {
-//     $set: {
-//       itensSold: { productId, quantity }
-//     }
-//   }))
-//   .then(() => ({ _id: id, itensSold: { productId, quantity } }));
+const updateSale = async (id, { itensSold: { productId, quantity } }) => connect()
+  .then((db) => db.collection('sales').updateOne({
+    _id: ObjectId(id)
+  }, {
+    $set: {
+      itensSold: { productId, quantity }
+    }
+  }))
+  .then(() => ({ _id: id, itensSold: { productId, quantity } }));
 
-// module.exports = {
-//   getAllSales,
-//   createSale,
-//   getSaleById,
-//   deleteSale,
-//   updateSale,
-// };
+module.exports = {
+  getAllSales,
+  createSale,
+  getSaleById,
+  deleteSale,
+  updateSale,
+};
--- services/salesService.js
+++ services/salesService.js
@@ -1,53 +1,53 @@
-// const salesModel = require('../models/salesModel');
-// // const sales = require('../controllers/salesController');
+const salesModel = require('../models/salesModel');
+// const sales = require('../controllers/salesController');
 
-// const validateSaleData = ({ itensSold: { productId, quantity } }) => {
-//   if (!productId) return { error: true, code: 'invalid_data', message: 'Produto inválido' };
-//   if (quantity <= 0) return {
-//     error: true, code: 'invalid_data', message: 'Quantidade deve ser maior que 0'
-//   };
-//   return { error: false };
-// };
+const validateSaleData = ({ itensSold: { productId, quantity } }) => {
+  if (!productId) return { error: true, code: 'invalid_data', message: 'Produto inválido' };
+  if (quantity <= 0) return {
+    error: true, code: 'invalid_data', message: 'Quantidade deve ser maior que 0'
+  };
+  return { error: false };
+};
 
-// const getAllSales = async () => salesModel.getAllSales();
+const getAllSales = async () => salesModel.getAllSales();
 
-// const createSale = async ({ itensSold: { productId, quantity } }) => {
-//   const validation = validateSaleData({ itensSold: { productId, quantity } });
+const createSale = async ({ itensSold: { productId, quantity } }) => {
+  const validation = validateSaleData({ itensSold: { productId, quantity } });
 
-//   if (validation.error) return validation;
+  if (validation.error) return validation;
 
-//   const sale = await salesModel.createSale({ itensSold: { productId, quantity } });
+  const sale = await salesModel.createSale({ itensSold: { productId, quantity } });
 
-//   return sale;
-// };
+  return sale;
+};
 
-// const getSaleById = async (id) => {
-//   const sale = await salesModel.getSaleById(id);
+const getSaleById = async (id) => {
+  const sale = await salesModel.getSaleById(id);
 
-//   if (!sale) throw new Error(`sale with id ${id} was not found`);
+  if (!sale) throw new Error(`sale with id ${id} was not found`);
 
-//   return sale;
-// };
+  return sale;
+};
 
-// const deleteSale = async (id) => {
-//   await salesModel.deleteSale(id);
-// };
+const deleteSale = async (id) => {
+  await salesModel.deleteSale(id);
+};
 
-// const updateSale = async (id, { itensSold: { productId, quantity } }) => {
-//   const validation = validateSaleData({ itensSold: { productId, quantity } });
-//   if (validation.error) return validation;
+const updateSale = async (id, { itensSold: { productId, quantity } }) => {
+  const validation = validateSaleData({ itensSold: { productId, quantity } });
+  if (validation.error) return validation;
 
-//   const sale = await getSaleById(id);
+  const sale = await getSaleById(id);
 
-//   if (sale.error) return sale;
+  if (sale.error) return sale;
 
-//   return salesModel.updateSale(id, { itensSold: { productId, quantity } });
-// };
+  return salesModel.updateSale(id, { itensSold: { productId, quantity } });
+};
 
-// module.exports = {
-//   getAllSales,
-//   createSale,
-//   getSaleById,
-//   deleteSale,
-//   updateSale,
-// };
+module.exports = {
+  getAllSales,
+  createSale,
+  getSaleById,
+  deleteSale,
+  updateSale,
+};
