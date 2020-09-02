2161c0b639a213abeff6f86f3e92ebf0eebf4194
Hfreitas <hfreitas86@gmail.com>
2020-09-02 16:52:53 -0300

    code climate

--- services/salesServices.js
+++ services/salesServices.js
@@ -2,27 +2,23 @@ const { createSales, getAllSales } = require('../models/salesModel');
 const { salesRegistryValidation } = require('./validation');
 
 const createSale = async (products = []) => {
-  try {
-    /* valida se alguma mensage de erro de validação é
+  /* valida se alguma mensage de erro de validação é
     retornada, caso sim inclui numa array de mensagens,
     é necessário tratar os erros para garantir que aplicação
     não quebre */
-    const productsValidation = products.reduce(async (acc, { productId, quantity }) => {
-      try {
-        if (Promise.reject) throw new Error('Wrong product ID or invalid quantity');
-        return !Promise.reject && salesRegistryValidation(productId, quantity)
-          ? [...acc, salesRegistryValidation(productId, quantity)]
-          : acc;
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    }, Promise.resolve([]));
-    const newSale = !productsValidation.length && createSales(products);
+  const productsValidation = products.reduce(async (acc, { productId, quantity }) => {
+    try {
+      if (Promise.reject) throw new Error('Wrong product ID or invalid quantity');
+      return !Promise.reject && salesRegistryValidation(productId, quantity)
+        ? [...acc, salesRegistryValidation(productId, quantity)]
+        : acc;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, Promise.resolve([]));
+  const newSale = !productsValidation.length && createSales(products);
 
-    return productsValidation || { ...newSale };
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  return productsValidation || { ...newSale };
 };
 
 const listSales = async () => {
