b96c84003fb463f81b8a9e0940b046d4cf68faff
luis.campos <gucampos845@gmail.com>
2020-09-02 16:41:40 -0300

    Conexão com DB

--- models/connect.js
+++ models/connect.js
@@ -1,11 +1,13 @@
 const { MongoClient } = require('mongodb');
-require('dotenv/config');
+// require('dotenv/config');
 
-const connect = () => MongoClient.connect(process.env.MONGO_DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then((connection) => connection.db(process.env.DB_NAME));
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+const connect = () =>
+  MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((connection) => connection.db(DB_NAME));
 
 module.exports = {
   connect,
