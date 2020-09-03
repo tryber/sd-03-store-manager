861d07dc5b6319c85f89999a8351843710c11338
Marco Barbosa <marco.meireles.b@gmail.com>
2020-09-02 21:14:04 -0300

    codeclimate rev2

--- services/productService.js
+++ services/productService.js
@@ -4,22 +4,26 @@ const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
   const sameProduct = await controllers.productsController.sameProduct(name);
   // validações
+  let response;
   if (typeof name !== 'string' || name.length < 5) {
-    return res.status(422).send({ err: { message: "\"name\" length must be at least 5 characters long", code: "invalid_data" } });
+    response = { err: { message: '\"name\" length must be at least 5 characters long', code: 'invalid_data' } };
   }
   if (sameProduct) {
-    return res.status(422).send({ err: { message: "Product already exists", code: "invalid_data" } });
+    response = { err: { message: 'Product already exists', code: 'invalid_data' } };
   }
   if (quantity <= 0) {
-    return res.status(422).send({ err: { message: "\"quantity\" must be larger than or equal to 1", code: "invalid_data" } });
+    response = { err: { message: '\"quantity\" must be larger than or equal to 1', code: 'invalid_data' } };
   }
   if (typeof quantity !== 'number') {
-    return res.status(422).send({ err: { message: "\"quantity\" must be a number", code: "invalid_data" } });
+    response = { err: { message: '\"quantity\" must be a number', code: 'invalid_data' } };
+  }
+  if (response) {
+    return res.status(422).send(response);
   }
 
   const result = await controllers.productsController.createProduct(name, quantity);
   return res.status(201).send(result);
-}; 
+};
 
 module.exports = {
   createProduct,
