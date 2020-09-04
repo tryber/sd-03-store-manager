a56c5cdd1bdfc482d3566703f712621d6d137834
Cardoso <lsc.cardosoo@gmail.com>
2020-09-04 11:17:23 -0300

    Projeto finalizado

--- services/productServices.js
+++ services/productServices.js
@@ -9,7 +9,6 @@ const {
 
 const handleCreateProduct = async (name, quantity) => {
   const product = await productModel.findProductByName(name);
-
   if (product) return { error: true, message: errMessage('invalid_data', PRODUCT_ALREADY_EXISTS) };
   return productModel.createProduct(name, quantity);
 };
