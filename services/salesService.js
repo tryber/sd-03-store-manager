eb63af2cacb1b33881edfa32701a70a5834ec68a
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-05 05:38:14 -0300

    first exercise

--- services/salesService.js
+++ services/salesService.js
@@ -5,8 +5,10 @@ const checkForHexRegExp = (id) => /^[0-9a-fA-F]{24}$/.test(id);
 
 const insertSales = async (saleInsert) => {
   const checkId = saleInsert.map((el) => !checkForHexRegExp(el.productId));
-  const checkNumber = saleInsert.map((el) => el.quantity <= 0);
-  if (checkId.some((el) => el === true) || checkNumber.some((el) => el === true)) {
+  const checkNumber = saleInsert.map((el) => el.quantity);
+  const checkNumberTwo = (el) => /^[0-9]+$/.test(el);
+  if (checkId.some((el) => el === true) || checkNumber.some((el) => el <= 0)
+  || checkNumber.some((el) => checkNumberTwo(el))) {
     return invaliddataError('Wrong product ID or invalid quantity');
   }
   const t = await sales.insertSales(saleInsert);
