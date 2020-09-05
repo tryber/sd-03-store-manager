4f99883a346070ef8486d8002e886cc01e93cc85
tales <talestales@gmail.com>
2020-09-05 00:00:26 -0300

    j

--- index.js
+++ index.js
@@ -14,21 +14,15 @@ app.get('/', (request, response) => {
 });
 
 app.post('/products', ProductController.productController);
-
 app.get('/products', ProductController.listProducts);
-
 app.get('/products/:id', ProductController.getProduct);
-
 app.put('/products/:id', ProductController.updateProduct);
-
 app.delete('/products/:id', ProductController.deleteProduct);
 
 app.post('/sales', SalesControler.createSale);
-
 app.get('/sales', SalesControler.listSales);
-
 app.get('/sales/:id', SalesControler.getSale);
 app.put('/sales/:id', SalesControler.updateSale);
 app.delete('/sales/:id', SalesControler.deleteSale);
 
-app.listen(3000, () => console.log('listen to port 3000'));
+app.listen(3000, () => console.log('Listen on port 3000'));
