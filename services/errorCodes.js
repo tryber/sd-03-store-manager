2b07c0991b26e47337f053c720cf981f3258b946
luizfernando <luizfernandotic@gmail.com>
2020-09-06 19:01:12 -0300

    helpers and error codes

---
+++ services/errorCodes.js
@@ -0,0 +1,11 @@
+const invalidData = 'invalid_data';
+const wrongFormat = 'Wrong id format';
+const wrongFormatOrQuantity = 'Wrong product ID or invalid quantity';
+const wrondFormatSaleId = 'Wrong sale ID format';
+
+module.exports = {
+  invalidData,
+  wrongFormat,
+  wrongFormatOrQuantity,
+  wrondFormatSaleId,
+};
