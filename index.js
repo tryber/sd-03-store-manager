581979ca174dac22218b9fb3cf5c7ecb202538be
luizfernando <luizfernandotic@gmail.com>
2020-09-06 19:01:41 -0300

    endpoints

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const { connect } = require('./models/connection');
+const { productsController, salesController } = require('./controllers');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-    response.send();
+  response.send();
 });
+
+app.get('/products', productsController.getAllProducts);
+app.get('/products/:id', productsController.getProductById);
+
+app.post('/products', productsController.createProduct);
+
+app.put('/products/:id', productsController.updateProduct);
+
+app.delete('/products/:id', productsController.deleteProduct);
+
+app.post('/sales', salesController.createSale);
+
+app.put('/sales/:id', salesController.updateSale);
+
+app.get('/sales', salesController.getAllSales);
+app.get('/sales/:id', salesController.getSaleById);
+
+app.delete('/sales/:id', salesController.deleteSale);
+
+/* connect().then((session) => {
+  console.log('Conectado ao mongodb!');
+}); */
+app.listen(3000, () => console.log('Ouvindo na porta 3000'));
