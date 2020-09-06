e9b14cf092e67a4e99a9715543c14bb23a073493
luizfernando <luizfernandotic@gmail.com>
2020-09-06 19:00:33 -0300

    services

---
+++ services/salesService.js
@@ -0,0 +1,65 @@
+const { salesModel, productsModel } = require('../models');
+const { validateId, validateSaleData, getErrorObject } = require('./helpers');
+const { invalidData, wrongFormatOrQuantity, wrondFormatSaleId } = require('./errorCodes');
+
+const createSale = (products) => {
+  const isDataValid = validateSaleData(products);
+
+  if (isDataValid) {
+    products.forEach(({ productId }) => {
+      const product = productsModel.getProductById(productId);
+
+      if (!product) getErrorObject(invalidData, wrongFormatOrQuantity);
+    });
+  }
+
+  if (!isDataValid) return getErrorObject(invalidData, wrongFormatOrQuantity);
+
+  const sale = salesModel.createSale(products);
+
+  return sale;
+};
+
+const getAllSales = async () => salesModel.getAllSales();
+
+const getSaleById = async (id) => {
+  const isIdValid = validateId(id);
+
+  if (typeof isIdValid === 'object') return isIdValid;
+
+  const sale = await salesModel.getSaleById(id);
+
+  if (!sale) return getErrorObject('not_found', 'Sale not found');
+
+  return sale;
+};
+
+const updateSale = async (id, products) => {
+  const isDataValid = validateSaleData(products);
+
+  if (!isDataValid) return getErrorObject(invalidData, wrongFormatOrQuantity);
+
+  const updatedSale = await salesModel.updateSale(id, products);
+
+  return updatedSale;
+};
+
+const deleteSale = async (id) => {
+  const isIdValid = validateId(id, wrondFormatSaleId);
+
+  if (typeof isIdValid === 'object') return isIdValid;
+
+  const sale = await salesModel.getSaleById(id);
+
+  if (!sale) return getErrorObject(invalidData, wrondFormatSaleId);
+
+  await salesModel.deleteSale(id);
+};
+
+module.exports = {
+  createSale,
+  getAllSales,
+  getSaleById,
+  updateSale,
+  deleteSale,
+};
