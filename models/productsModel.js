181a5bf4f347744affd37d1a32d625398711ae55
luizfernando <luizfernandotic@gmail.com>
2020-09-06 18:59:07 -0300

    models

---
+++ models/productsModel.js
@@ -0,0 +1,39 @@
+const { ObjectId } = require('mongodb');
+const { connect } = require('./connection');
+
+const createProduct = async (name, quantity) =>
+  connect()
+    .then((db) => db.collection('products').insertOne({ name, quantity }))
+    .then(({ insertedId }) => ({ _id: insertedId, name, quantity }));
+
+const getProductByName = async (searchName) =>
+  connect().then((db) => db.collection('products').findOne({ name: searchName }));
+
+const getAllProducts = async () =>
+  connect()
+    .then((db) => db.collection('products').find({}).toArray())
+    .then((products) => ({ products }));
+
+const getProductById = async (searchId) =>
+  connect().then((db) => db.collection('products').findOne(ObjectId(searchId)));
+
+const updateProduct = async (id, name, quantity) =>
+  connect()
+    .then((db) =>
+      db.collection('products').updateOne({ _id: ObjectId(id) }, { $set: { name, quantity } }),
+    )
+    .then(() => ({ _id: id, name, quantity }));
+
+const deleteProduct = async (id) =>
+  connect()
+    .then((db) => db.collection('products').deleteOne({ _id: ObjectId(id) }))
+    .then((deleted) => deleted);
+
+module.exports = {
+  createProduct,
+  getProductByName,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
