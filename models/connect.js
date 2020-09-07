be611a9ec170f79c60be35f6251309de545e3b75
luis.campos <gucampos845@gmail.com>
2020-09-07 10:47:20 -0300

    Correção de conexão

--- models/connect.js
+++ models/connect.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv/config');
 
 const { MONGO_DB_URL, DB_NAME } = process.env;
-const dbUrl = MONGO_DB_URL || 'mongodb://localhost:27017/StoreManager';
+const dbUrl = MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const dbName = DB_NAME || 'StoreManager';
 
 const connect = () =>
