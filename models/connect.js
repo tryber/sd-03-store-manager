54a00073076da45ba09ae4fe44994ba8be3c8da0
DevT <aguiarthiag@gmail.com>
2020-09-07 04:52:59 -0300

    CC v666 com variáveis MongoDB corretas

--- models/connect.js
+++ models/connect.js
@@ -4,11 +4,11 @@ const { MongoClient } = require('mongodb');
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
-const connect = async () => MongoClient.connect('mongodb://localhost:27017/StoreManager', {
+const connect = async () => MongoClient.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then((connection) => connection.db('StoreManager'))
+  .then((connection) => connection.db(DB_NAME))
   .catch((err) => {
     console.error(err);
     process.exit(1);
