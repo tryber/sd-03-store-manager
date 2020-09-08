f231af2d40e6896f1e98d496955aeddfb09d752a
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-08 08:25:46 -0300

    laal

--- services/salesService.js
+++ services/salesService.js
@@ -5,6 +5,23 @@ const { invaliddataError, notFound } = require('../errors');
 const checkForHexRegExp = (id) => /^[0-9a-fA-F]{24}$/.test(id);
 const checkNumberTwo = (el) => /^[0-9]+$/.test(el);
 
+const deleteSales = async (id) => {
+  if (!checkForHexRegExp(id)) {
+    return invaliddataError('Wrong sale ID format');
+  }
+  const sale = await sales.getSalesById(id);
+  if (!sale) {
+    return notFound('Sale not found');
+  }
+  Promise.all(
+    await sale.itensSold.map((el) => products.getProductById(el.productId)),
+  ).then((e) => e.map((el, index) => products
+    .updateProduct(sale.itensSold[index].productId,
+      el.name, el.quantity + sale.itensSold[index].quantity)));
+  await sales.deleteSales(id);
+  return sale;
+};
+
 const insertSales = async (saleInsert) => {
   const checkId = saleInsert.map((el) => !checkForHexRegExp(el.productId));
   const checkNumber = saleInsert.map((el) => el.quantity);
@@ -12,12 +29,24 @@ const insertSales = async (saleInsert) => {
   || checkNumber.some((el) => !checkNumberTwo(el))) {
     return invaliddataError('Wrong product ID or invalid quantity');
   }
+  Promise.all(await saleInsert.map((el) => products.getProductById(el.productId)))
+    .then((e) => {
+      for (let i = 0; i <= e.length - 1; i += 1) {
+        if (e[i].quantity - saleInsert[i].quantity <= 0) {
+          this.f = true;
+        }
+      }
+    });
   Promise.all(
     await saleInsert.map((el) => products.getProductById(el.productId)),
   ).then((e) => e.map((el, index) => products
     .updateProduct(saleInsert[index].productId,
       el.name, el.quantity - saleInsert[index].quantity)));
   const t = await sales.insertSales(saleInsert);
+  if (this.f) {
+    await deleteSales(Object.values(t)[0]);
+    return notFound('Such amount is not permitted to sell');
+  }
   return t;
 };
 
@@ -42,23 +71,6 @@ const updateSales = async (id, values) => {
   }
   const allSales = await sales.updateSales(id, values);
   return allSales;
-};
-
-const deleteSales = async (id) => {
-  if (!checkForHexRegExp(id)) {
-    return invaliddataError('Wrong sale ID format');
-  }
-  const sale = await sales.getSalesById(id);
-  if (!sale) {
-    return notFound('Sale not found');
-  }
-  Promise.all(
-    await sale.itensSold.map((el) => products.getProductById(el.productId)),
-  ).then((e) => e.map((el, index) => products
-    .updateProduct(sale.itensSold[index].productId,
-      el.name, el.quantity + sale.itensSold[index].quantity)));
-  await sales.deleteSales(id);
-  return sale;
 };
 
 module.exports = {
