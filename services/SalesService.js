47fa3ea1b08085f5ddbe3cbfd3704bb0c78a026f
Clayton Pereira <cpereiramt@gmail.com>
2020-09-08 19:01:08 -0400

    Salvando alteraçoes CC 2 requisito 5

--- services/SalesService.js
+++ services/SalesService.js
@@ -1,9 +1,10 @@
 const productsModel = require('../models/Products');
 const salesModel = require('../models/Sales');
+
 const invalid = 'invalid_data';
 let code;
 let message;
-const stock_error = 'stock_problem';
+const stockError = 'stock_problem';
 
 const validateSaleCreate = ({ productId, quantity }) => {
   return quantity >= 1 && typeof quantity === 'number' && productId.length === 24;
@@ -11,33 +12,27 @@ const validateSaleCreate = ({ productId, quantity }) => {
 
 const saleCreate = async (products) => {
   let validation;
-  await products.forEach((product) => {
-    validation = validateSaleCreate(product);
-  });
-  console.log(validation);
+  await products.forEach((product) => validation = validateSaleCreate(product));
   if (!validation) {
     code = invalid;
     message = 'Wrong product ID or invalid quantity';
     return { err: { code, message } };
-  }
-
+ }
   if (validation) {
     products.forEach(async ({ productId, quantity }) => {
       const productCreated = await productsModel.ProductById(productId);
       let stock = productCreated.quantity;
-
       if (!productCreated) {
         code = invalid;
         message = 'Wrong';
         return { err: { code, message } };
       }
-
       if (quantity > stock) {
-        code = stock_error;
+        code = stockError;
         message = 'Such amount is not permitted to sell';
         return { err: { code, message } };
-      }
-    });
+      }}
+    );
   }
   console.log('criou com sucesso !');
   const sale = salesModel.saleCreate(products);
