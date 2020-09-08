a275ebe0dd2d222ede91f1c0e6d18bc515c3ce2a
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-09-08 09:51:19 -0300

    Code Climate

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,12 @@
 const rescue = require('express-rescue');
 const productService = require('../services/productService');
 
-const listAllProducts = rescue(async (req, res) => {
+const error422 = (res, result) => res.status(422).json({ err: { code: 'invalid_data', message: result.message } });
+
+const listAllProducts = rescue(async (_req, res) => {
   const result = await productService.getAll();
 
-  if (result.error) return res.status(422).json({ err: { code: 'invalid_data', message: result.message } });
+  if (result.error) return error422(res, result);
 
   res.status(201).json(result);
 });
@@ -14,7 +16,7 @@ const findProduct = rescue(async (req, res) => {
 
   const result = await productService.getById(id);
 
-  if (result.error) return res.status(422).json({ err: { code: 'invalid_data', message: result.message } });
+  if (result.error) return error422(res, result);
 
   res.status(200).json(result);
 });
@@ -23,7 +25,7 @@ const addProduct = rescue(async (req, res) => {
   const { name, quantity } = req.body;
   const result = await productService.addProduct({ name, quantity });
 
-  if (result.error) return res.status(422).json({ err: { code: 'invalid_data', message: result.message } });
+  if (result.error) return error422(res, result);
 
   res.status(200).json(result);
 });
