5453e17d65874d063911e7ad4730fe3c18913dfd
luldelrey <rpgstrikered@gmail.com>
2020-09-13 00:37:26 -0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-    response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
