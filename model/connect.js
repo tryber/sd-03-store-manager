0e12888be7aa954bff43c104f574a72991ab2aa8
potew <potew0@gmail.com>
2020-09-16 19:27:06 -0300

    Connect fix

--- model/connect.js
+++ model/connect.js
@@ -4,11 +4,11 @@ const { MongoClient } = require('mongodb');
 const { DB_URI, DB_DBNAME } = process.env;
 
 const connect = async () => MongoClient.connect(
-  DB_URI, {
+  'mongodb://mongodb:27017/StoreManager', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // As novas regras que o team do MongoDB criou,
     // mas ficam desativadas por default para fins de retrocompatibilidade
-  }).then((connection) => connection.db(DB_DBNAME));
+  }).then((connection) => connection.db('StoreManager'));
 
 module.exports = connect;
