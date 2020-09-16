689ac822b9fa2b8941185c3e37faa6c774e3b965
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-09-16 08:36:20 -0300

    Adicionando funçoes de validação de dados

--- models/productsModel.js
+++ models/productsModel.js
@@ -6,6 +6,24 @@ const getAllProducts = async () => connect()
     .find({})
     .toArray());
 
+const createProduct = async (name, quatity) => connect()
+  .then((db) => db
+    .collection('products')
+    .insertOne({ name, quatity }))
+  .then(({ insertedId }) => ({
+    _id: insertedId,
+    name,
+    quatity,
+  }));
+
+const getProductByName = async (name) => connect()
+  .then((db) => db
+    .collection('products')
+    .find({"name": name})
+    .toArray());
+
 module.exports = {
   getAllProducts,
+  createProduct,
+  getProductByName,
 };
---
+++ utils/productValidation.js
@@ -0,0 +1,60 @@
+const { productsModel } = require('../models');
+
+const nameValidation = async (productName) => {
+  const name = await productsModel.getProductByName(productName);
+
+  switch (true) {
+    case productName.length < 5:
+      return {
+        "err": {
+          "code": "invalid_data",
+          "message": "\"name\" length must be at least 5 characters long"
+        }
+      }
+    case name.length > 0:
+      return {
+        "err": {
+          "code": "invalid_data",
+          "message": "Product already exists"
+        }
+      }
+    default:
+      return true;
+  }
+};
+
+const quantityValidation = (productQuantity) => {
+  switch (true) {
+    case productQuantity <= 0:
+      return {
+        "err": {
+          "code": "invalid_data",
+          "message": "\"quantity\" must be larger than or equal to 1"
+        }
+      }
+    case typeof(productQuantity) === 'string':
+      return {
+        "err": {
+          "code": "invalid_data",
+          "message": "\"quantity\" must be a number"
+        }
+      }
+    default:
+      return true;
+  }
+};
+
+const productValidation = async (name, quantity) => {
+  const productNameValidation = await nameValidation(name);
+  const productQuantityValidation = quantityValidation(quantity);
+
+  if (productNameValidation.err) return productNameValidation;
+  
+  if (productQuantityValidation) return productQuantityValidation;
+  
+  return true;
+};
+
+module.exports = {
+  productValidation,
+};
