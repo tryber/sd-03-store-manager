65c2e855e5fdb57e623086fca900847237fbacb0
rdgbrezolin <brezolinr@gmail.com>
2020-09-17 16:37:37 -0300

    connection fail

---
+++ models/salesModel.js
@@ -0,0 +1,54 @@
+const ObjectId = require('mongodb').ObjectID;
+const connect = require('./connection');
+
+const registerSales = async (sales) =>
+  connect()
+    .then((db) => db.collection('sales').insertOne({ itensSold: sales }))
+    .then(({ insertedId }) => ({ _id: insertedId, itensSold: sales }));
+
+const getSaleById = async (id) =>
+  connect()
+    .then((db) => db.collection('sales').findOne(ObjectId(id)))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+const getAllSales = async () =>
+  connect()
+    .then((db) => db.collection('sales').find({}).toArray())
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+const updateSale = async (id, { productId, quantity }) =>
+  connect()
+    .then((db) => db.collection('sales').updateOne(
+      { _id: id },
+      { $set: { 'itensSold.$[id].productId': productId, 'itensSold.$[qtd].quantity': quantity } },
+      {
+        arrayFilters: [{ 'id.productId': productId }, { 'qtd.quantity': quantity }],
+      },
+    ))
+    .then(() => ({ _id: id, itensSold: [{ productId, quantity }] }))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+const deleteSale = async (id) =>
+  connect()
+    .then((db) => db.collection('sales').deleteOne({ _id: ObjectId(id) }))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+module.exports = {
+  registerSales,
+  getSaleById,
+  getAllSales,
+  updateSale,
+  deleteSale,
+};
