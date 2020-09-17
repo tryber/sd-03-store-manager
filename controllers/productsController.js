4eb0271ce6b2a4998b69c568c53a66d93320069e
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-09-16 23:02:01 -0300

    cc

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -38,12 +38,12 @@ const updateProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
-  const product = await productsService.deleteProduct(id)
+  const product = await productsService.deleteProduct(id);
 
   if (product && product.err) return res.status(422).json(product);
 
   return res.status(200).end();
-}
+};
 
 module.exports = {
   getAll,
