747500cf081914ccb10db518aafe30ba893f8a28
luldelrey <rpgstrikered@gmail.com>
2020-09-17 22:32:44 -0300

    saving progress

--- models/salesM.js
+++ models/salesM.js
@@ -1,24 +1,24 @@
 // MODEL: Recebe dados tratados do service e interage com o banco de dados!
-const { ObjectId } = require('mongodb');
-const connection = require('./connection');
+// const { ObjectId } = require('mongodb');
+// const connection = require('./connection');
 
-const createProduct = async (name, quantity) => connection();
+// const createProduct = async (name, quantity) => connection();
 
-const deleteProduct = async (id) => connection();
+// const deleteProduct = async (id) => connection();
 
-const getAllProducts = async () => connection();
+// const getAllProducts = async () => connection();
 
-const getProductsById = async (id) => connection();
+// const getProductsById = async (id) => connection();
 
-const getProductsByName = async (name) => connection();
+// const getProductsByName = async (name) => connection();
 
-const updateProduct = async (id, name, quantity) => connection();
+// const updateProduct = async (id, name, quantity) => connection();
 
-module.exports = {
-  createProduct,
-  deleteProduct,
-  getAllProducts,
-  getProductsById,
-  getProductsByName,
-  updateProduct,
-};
+// module.exports = {
+//   createProduct,
+//   deleteProduct,
+//   getAllProducts,
+//   getProductsById,
+//   getProductsByName,
+//   updateProduct,
+// };
