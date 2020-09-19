8a186adc628bb19ff7f4c587215952ac3d620d95
mirellaalves <mirella.alves@gmail.com>
2020-09-19 20:00:52 -0300

    inicial commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-    response.send();
+app.get('/', (req, res) => {
+    res.send();
 });
+
+app.listen(3000, () => console.log('Listening on 3000'));
