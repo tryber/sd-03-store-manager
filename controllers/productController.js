418edb3a893cbd4ff6fb3f3e02e6ce3a148f0e03
mirellaalves <mirella.alves@gmail.com>
2020-09-22 22:00:05 -0300

    code climate issues

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,11 +5,11 @@ const products = Router();
 
 products.post('/', async (req, res, _next) => {
   const { name, quantity } = req.body;
-  const product = await productService.createProduct(name, quantity);
-  if (product.error) {
-    return res.status(422).json({ code: 'invalid_data', message: product.message });
+  const postProduct = await productService.createProduct(name, quantity);
+  if (postProduct.error) {
+    return res.status(422).json({ code: 'invalid_data', message: postProduct.message });
   }
-  return res.status(201).json(product);
+  return res.status(201).json(postProduct);
 });
 
 products.get('/', async (_, res) => {
@@ -19,11 +19,11 @@ products.get('/', async (_, res) => {
 
 products.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await productService.getProductById(id);
-  if (product.error) {
-    return res.status(422).json({ code: 'invalid_data', message: product.message });
+  const getProduct = await productService.getProductById(id);
+  if (getProduct.error) {
+    return res.status(422).json({ code: 'invalid_data', message: getProduct.message });
   }
-  return res.status(200).json(product);
+  return res.status(200).json(getProduct);
 });
 
 module.exports = products;
