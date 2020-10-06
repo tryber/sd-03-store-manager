f761850f7c9ae4d8103866c0a1a2346464132b4a
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-10-06 06:19:47 -0300

    model updatado

--- models/productsModel.js
+++ models/productsModel.js
@@ -12,17 +12,64 @@ const registerProducts = async (name, quantity) => {
   }
 };
 
-const listProducts = async () => {
+const updateProductById = async (id, productName, productQuantity) => {
   try {
     const connect = await connection('products');
-    const lister = await connect.find();
-    return lister;
+    const updateQuery = await connect.findOneAndUpdate(
+      { _id: ObjectID(id) },
+      { $set: { name: productName, quantity: productQuantity } },
+      { returnOriginal: false },
+    ); 
+    return updateQuery.value;
   } catch (error) {
-    throw new Error(error.message || 'product query failed');
+    throw new Error(error.message || 'product update failed');
+  }
+};
+
+const deleteProductById = async (id) => {
+  try {
+    const deleteQuery = await connectAndDeleteById(id, 'products', 'Wrong id format');
+    const { _id, name, quantity } = deleteQuery;
+  
+    return { _id, name, quantity };
+  } catch (error) {
+    throw new Error('Wrong id format');
+  }
+};
+  
+const getAllProducts = async () => {
+  try {
+    const searchAllProducts = await connectAndFindAll('products');
+    return searchAllProducts;
+  } catch (error) {
+    throw new Error('products search failed');
+  }
+};
+
+const getProductByName = async (name) => {
+  try {
+    const connect = await connection('products');
+    const searchQuery = connect.findOne({ name });
+    return searchQuery;
+  } catch (error) {
+    throw new Error('product search failed');
+  }
+};
+
+const getProductById = async (id) => {
+  try {
+    const query = connectAndFindById(id, 'products', 'Wrong id format');
+    return query;
+  } catch (error) {
+    throw new Error('Wrong id format');
   }
 };
 
 module.exports = {
   registerProducts,
-  listProducts,
+  updateProductById,
+  deleteProductById,
+  getAllProducts,
+  getProductByName,
+  getProductById,
 };
